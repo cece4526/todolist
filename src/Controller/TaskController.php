eec8093dd18a38ef2eaa4ff3c411c53de169bd2a
cece4526 <cece4526@hotmail.fr>
2023-10-27 17:38:52 +0200

    create task fonctionnel

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,18 +2,68 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
+    #[Route('/tasks', name: 'app_task')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TaskController.php',
+        ]);
+    }
+
+    #[Route('/tasks/new', name: 'task_create', methods: ['GET', 'POST'])]
+    public function new(Request $request, TaskRepository $taskRepository,EntityManagerInterface $em, SluggerInterface $slugger): Response
+    {
+
+        //I create my form for new task
+        $task = new Task();
+    // $this->denyAccessUnlessGranted('TASK_CREATE', $task);
+        $form = $this->createForm(TaskType::class, $task);
+        // $user = $this->getUser();
+        // dd($form, $request);
+        //the form request is processed
+        $form->handleRequest($request);
+
+        // if ($user === null) {
+            
+        //     $this->addFlash('danger', 'Veuillez vous connecter pour ajouter un task');
+        //     return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        // }
+        //I check if I have a form and that it is valid
+        if ($form->isSubmitted() && $form->isValid()) {
+            $now = new DateTimeImmutable();
+            $task->setCreatedAt($now);
+            $task->setTitle(strtoupper($task->getTitle()));
+            // $task->setAuthor($user);
+            $em->persist($task);
+            $em->flush();
+
+            $taskRepository->save($task, true);
+            $this->addFlash(
+                'success',
+                'Le task a bien été enregistré'
+            );
+
+            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('tasks/create.html.twig', [
+            'task' => $task,
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/users', name: 'app_user')]
     public function index(): JsonResponse
     {
         return $this->json([
